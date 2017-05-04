acdebef98c0a847a467e332f5b7530e56ad82f69
Zane <pzwoodfin@gmail.com>
2017-05-03 22:31:17 -0400

    Adds configuration for dock monitors and autorepeat caps-backspace.

--- bin/bin/enable_monitors.sh
+++ bin/bin/enable_monitors.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 if type "xrandr" > /dev/null; then
-  # enable monitor through insignia usb-c to hdmi adapter
-  xrandr --addmode DP1 1920x1080
   # enable external monitors
-  xrandr --output eDP1 --auto --output HDMI2 --auto --right-of eDP1 --output DP1 --auto --left-of eDP1
+  xrandr --output eDP1 --primary --output DP2-1 --left-of eDP1 --output DP2-2 --right-of eDP1
 else
   echo "ERROR: xrandr is not installed!"
 fi
