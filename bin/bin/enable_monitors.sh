b27706a4c2612594f9b02226549f93a60f4375dc
Zane <pzwoodfin@gmail.com>
2017-05-11 19:35:03 -0400

    Adds cpu and temp to status bar and adds dock monitor config.

--- bin/bin/enable_monitors.sh
+++ bin/bin/enable_monitors.sh
@@ -2,7 +2,7 @@
 
 if type "xrandr" > /dev/null; then
   # enable external monitors
-  xrandr --output eDP1 --primary --output DP2-1 --left-of eDP1 --output DP2-2 --right-of eDP1
+  xrandr --output DP2-1 --output eDP1 --primary --right-of DP2-1 --output DP2-2 --right-of eDP1
 else
   echo "ERROR: xrandr is not installed!"
 fi
